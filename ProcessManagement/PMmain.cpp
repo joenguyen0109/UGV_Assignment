57694cda6e03e1fe27600d9f33c522e82fb42a6d
Joe Nguyen <47797190+joenguyen0109@users.noreply.github.com>
2021-09-28 11:56:09 +1000

    add SM for PM

--- ProcessManagement/PMmain.cpp
+++ ProcessManagement/PMmain.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #using <System.dll>
 #include <conio.h>//_kbhit()
+#include <UGV_module.h>
+
+#include <smstructs.h>
+
 using namespace System;
 using namespace System::Diagnostics;
 using namespace System::Threading;
@@ -11,6 +15,15 @@ int main() {
 	array<String^>^ moduleList = gcnew array<String^>{"GPS"};
 	array<int>^ criticalList = gcnew array<int>(moduleList->Length) { 0 };
 	array<Process^>^ processList = gcnew array<Process^>(moduleList->Length);
+
+
+	SMObject MonitorDataSMObj(_TEXT("MonitorData"), sizeof(ProcessManagement));
+	MonitorDataSMObj.SMCreate();
+	MonitorDataSMObj.SMAccess();
+
+	ProcessManagement* dataPointer = (ProcessManagement*)MonitorDataSMObj.pData;
+	dataPointer->Heartbeat.Status = 0;
+	dataPointer->Shutdown.Status = 0;
 
 	for (int i = 0; i < moduleList->Length; i++) {
 		if (Process::GetProcessesByName(moduleList[i])->Length == 0) {
@@ -24,6 +37,8 @@ int main() {
 	while (!_kbhit()) {
 		Thread::Sleep(1000);
 	}
+
+	dataPointer->Shutdown.Status = 0xFF;
 	Console::ReadKey();
 	return 0;
 }
