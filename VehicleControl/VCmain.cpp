a5a17b87af0dbca7ef66bd6e3e4499ebd68128e6
Joe Nguyen <47797190+joenguyen0109@users.noreply.github.com>
2021-09-29 11:14:46 +1000

    test out laser tcp

--- VehicleControl/VCmain.cpp
+++ VehicleControl/VCmain.cpp
@@ -1,9 +1,73 @@
-#include <conio.h>//_kbhit()
+//Compile in a C++ CLR empty project
 #using <System.dll>
-int main() {
-	while (!_kbhit()) {
 
-		System::Threading::Thread::Sleep(1000);
+#include <conio.h>//_kbhit()
+
+using namespace System;
+using namespace System::Net::Sockets;
+using namespace System::Net;
+using namespace System::Text;
+
+int main()
+{
+	// LMS151 port number must be 2111
+	int PortNumber = 23000;
+	// Pointer to TcpClent type object on managed heap
+	TcpClient^ Client;
+	// arrays of unsigned chars to send and receive data
+	array<unsigned char>^ SendData;
+	array<unsigned char>^ ReadData;
+	// String command to ask for Channel 1 analogue voltage from the PLC
+	// These command are available on Galil RIO47122 command reference manual
+	// available online
+	String^ AskScan = gcnew String("sRN LMDscandata");
+	// String to store received data for display
+	String^ ResponseData;
+
+	// Creat TcpClient object and connect to it
+	Client = gcnew TcpClient("192.168.1.200", PortNumber);
+	// Configure connection
+	Client->NoDelay = true;
+	Client->ReceiveTimeout = 500;//ms
+	Client->SendTimeout = 500;//ms
+	Client->ReceiveBufferSize = 1024;
+	Client->SendBufferSize = 1024;
+
+	// unsigned char arrays of 16 bytes each are created on managed heap
+	SendData = gcnew array<unsigned char>(16);
+	ReadData = gcnew array<unsigned char>(2500);
+	// Convert string command to an array of unsigned char
+	SendData = System::Text::Encoding::ASCII->GetBytes(AskScan);
+
+
+	// Get the network streab object associated with clien so we 
+	// can use it to read and write
+	NetworkStream^ Stream = Client->GetStream();
+
+
+	//Loop
+	while (!_kbhit())
+	{
+		// Write command asking for data
+		Stream->WriteByte(0x02);
+		Stream->Write(SendData, 0, SendData->Length);
+		Stream->WriteByte(0x03);
+		// Wait for the server to prepare the data, 1 ms would be sufficient, but used 10 ms
+		System::Threading::Thread::Sleep(10);
+		// Read the incoming data
+		Stream->Read(ReadData, 0, ReadData->Length);
+		// Convert incoming data from an array of unsigned char bytes to an ASCII string
+		ResponseData = System::Text::Encoding::ASCII->GetString(ReadData);
+		// Print the received string on the screen
+		Console::WriteLine(ResponseData);
 	}
+
+	Stream->Close();
+	Client->Close();
+
+	Console::ReadKey();
+	Console::ReadKey();
+
+
 	return 0;
 }
